e80d07adb4a1cd273b5e254cb75798e5a948ff89
Will <wmurdoch@brownsteingroup.com>
2018-02-16 16:07:34 -0500

    Started data structure

--- index.php
+++ index.php
@@ -14,7 +14,19 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
 
-  
+  <div id="projects">
+    <?php
+    $json = file_get_contents('js/projects.json');
+    $projects = json_decode($json, true);
+    foreach($projects as $project):
+      echo '<pre>';
+      print_r($project);
+      echo '</pre>';
+      echo $project['title'];
+      //echo $project['title'];
+    endforeach;
+    ?>
+  </div>
 
   <script src="js/vendor/modernizr-2.8.3.min.js"></script>
   <script src="js/vendor/jquery-1.12.0.min.js"></script>
