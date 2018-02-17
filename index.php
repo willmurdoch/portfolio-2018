60ea783d88ce126d7417a95e4012a63d799a183a
willm <willmurdoch@gmail.com>
2018-02-17 04:22:07 -0500

    Parallaxy stuff

--- index.php
+++ index.php
@@ -13,36 +13,38 @@
   <!--[if lt IE 8]>
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
-
-  <section id="projects">
-    <?php
-    $json = file_get_contents('projects.json');
-    $projects = json_decode($json, true);
-    $count = 0;
-    foreach($projects as $project): ?>
-      <article class="project">
-        <div class="textWrap">
-          <span class="count"><?php $count++; if($count < 10) echo '0'.$count; else echo $count; ?></span>
-          <h3><?php echo $project['title']; ?></h3>
-          <p><?php echo $project['description']; ?></p>
-          <?php if(isset($project['link'])): ?>
-            <a class="cta" href="<?php echo $project['link']; ?>" target="_blank">Visit Site</a>
-          <?php endif; ?>
-        </div>
-        <div class="imageWrap">
-          <?php if(isset($project['desktop'])): ?>
-            <div class="desktop">
-              <img src="<?php echo $project['desktop']; ?>" alt="<?php echo $project['title'] ?> Desktop Preview" />
-            </div>
-          <?php endif; if(isset($project['mobile'])): ?>
-            <div class="mobile">
-              <img src="<?php echo $project['mobile']; ?>" alt="<?php echo $project['title'] ?> Mobile Preview" />
-            </div>
-          <?php endif; ?>
-        </div>
-      </article>
-    <?php endforeach; ?>
-  </section>
+  <div id="wrapper">
+    <section id="projects">
+      <?php
+      $json = file_get_contents('projects.json');
+      $projects = json_decode($json, true);
+      $count = 0;
+      foreach($projects as $project): ?>
+        <article class="project">
+          <div class="textWrap">
+            <span class="count"><?php $count++; if($count < 10) echo '0'.$count; else echo $count; ?></span>
+            <h3><?php echo $project['title']; ?></h3>
+            <p><?php echo $project['description']; ?></p>
+            <?php if(isset($project['link'])): ?>
+              <a class="cta" href="<?php echo $project['link']; ?>" target="_blank">Visit Site</a>
+            <?php endif; ?>
+          </div>
+          <div class="imageWrap">
+            <?php if(isset($project['desktop'])): ?>
+              <div class="desktop">
+                <img src="<?php echo $project['desktop']; ?>" alt="<?php echo $project['title'] ?> Desktop Preview" />
+              </div>
+            <?php endif; if(isset($project['mobile'])): ?>
+              <div class="mobile">
+                <img src="<?php echo $project['mobile']; ?>" alt="<?php echo $project['title'] ?> Mobile Preview" />
+              </div>
+            <?php endif; ?>
+          </div>
+          <span class="circle"></span>
+        </article>
+      <?php endforeach; ?>
+    </section>
+  </div>
 
   <script src="js/vendor/modernizr-2.8.3.min.js"></script>
   <script src="js/vendor/jquery-1.12.0.min.js"></script>
--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ $('.desktop').click(function(){
 $('.mobile').click(function(){
   $(this).removeClass('out').toggleClass('in').siblings().toggleClass('out').removeClass('in');
 });
+
+function setCircles(){
+  $('.circle').each(function(){
+    var mySpot = $(this).parents('.project').offset().top - $(window).scrollTop();
+    mySpot = mySpot/5;
+    $(this).css('top', 'calc(50% - '+mySpot+'px)');
+    $(this).siblings('.textWrap').css('transform', 'translateY('+mySpot/2+'px)');
+    $(this).siblings('.imageWrap').find('.mobile img').css('transform', 'translateY('+mySpot+'px)');
+    $(this).siblings('.imageWrap').find('.desktop img').css('transform', 'translateY('+mySpot/3+'px)');
+  });
+}
+
+$(document).ready(function(){
+  setCircles();
+});
+
+$(window).scroll(function(){
+  setCircles();
+});
