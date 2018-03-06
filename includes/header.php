4197563011f8f2a935b8509d0ed0b512e0e311d6
Will <wmurdoch@brownsteingroup.com>
2018-03-06 13:10:20 -0500

    Changed work text/order

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Will Murdoch</title>
-  <meta name="description" content="">
+  <meta name="description" content="I make things for the web.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="favicon.ico">
   <link rel="stylesheet" href="css/main.css">
